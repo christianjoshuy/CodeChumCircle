1b5828133cc3db940e9637a8d49eaa62cc55d5f7
Dell <londonuts213@gmail.com>
2022-12-12 11:24:02 +0800

    Added Solution

--- src/CircleGUI.java
+++ src/CircleGUI.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class CircleGUI extends JFrame{
     private JPanel pnlMain;
@@ -7,6 +9,28 @@ public class CircleGUI extends JFrame{
     private JTextField tfCircumference;
     private JButton btnClear;
     private JButton btnCompute;
+
+    public CircleGUI(){
+        btnCompute.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                double radius = Double.parseDouble(tfRadius.getText());
+                double area = Math.PI * Math.pow(radius, 2);
+                double circumference = 2 * Math.PI * radius;
+                tfArea.setText(String.format("%.2f", area));
+                tfCircumference.setText(String.format("%.2f", circumference));
+            }
+        });
+
+        btnClear.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                tfRadius.setText("");
+                tfArea.setText("");
+                tfCircumference.setText("");
+            }
+        });
+    }
 
     public static void main(String[] args) {
         CircleGUI app = new CircleGUI();
